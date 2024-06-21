dc8c0da8fbd88fd2ce265a562adf1a2462563ed3
Ruslan Russkikh <dvrussk@yandex.ru>
2024-06-21 12:13:39 +0300

    Add license plugin and oil.nvim

--- lua/kekulta/lazy/lsp.lua
+++ lua/kekulta/lazy/lsp.lua
@@ -111,13 +111,6 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if ls.expand_or_jumpable() then
-                            ls.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
 
                     ["<S-Tab>"] = cmp.mapping(function(fallback)
                         if ls.jumpable(-1) then
---
+++ lua/kekulta/lazy/sl.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'https://github.com/antoyo/vim-licenses',
+        name = 'software-licenses',
+        config = function()
+            vim.g.licenses_authors_name = 'RUSSKIKH, RUSLAN dvrussk@yandex.ru'
+            vim.g.licenses_copyright_holders_name = 'RUSSKIKH, RUSLAN dvrussk@yandex.ru'
+        end
+    }
+}
