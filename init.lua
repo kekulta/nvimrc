33b28a31e540d09ce3aa467bd6ff2d08e80607dd
Ruslan Russkikh <dvrussk@yandex.ru>
2024-10-01 18:45:01 +0300

    Strip down bloat

--- init.lua
+++ init.lua
@@ -1,1 +1,62 @@
-require("kekulta")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git clone https://github.com/folke/lazy.nvim.git --branch=stable --filter=blob:none", lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        { 'stevearc/oil.nvim', },
+        { "catppuccin/nvim", name = "catppuccin", },
+    },
+    change_detection = { notify = false }
+})
+
+require("oil").setup()
+vim.cmd.colorscheme("catppuccin-macchiato")
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.keymap.set({"t"}, "<Esc>", "<C-\\><C-n>")
+
+function send_to_qflist(_, data, _) 
+    for _, s in ipairs(data) do
+        local escaped = s:gsub('"', '\\"')
+        vim.cmd('caddexpr "' .. escaped .. '"')
+        print(s)
+    end
+end
+
+compile_job = nil
+
+vim.api.nvim_create_user_command('Comp',
+  function(opts)
+    vim.cmd('cgetexpr system("echo Compiling...")')
+    vim.cmd('copen')
+
+    if compile_job ~= nil then
+        vim.fn.jobstop(compile_job)
+    end
+
+    compile_job = vim.fn.jobstart(opts.args , {
+         on_stdout = send_to_qflist,
+         on_stderr = send_to_qflist,
+         on_exit = function(_, _, _) vim.cmd('caddexpr "' .. 'Compiled!' .. '"') end,
+         })
+     end,
+  
+  { nargs = 1, 
+  complete = function(_, cmdline)
+		local cmd = cmdline:gsub("Comp%s+", "")
+		local results = vim.fn.getcompletion(("!%s"):format(cmd), "cmdline")
+		return results
+	end,
+})
